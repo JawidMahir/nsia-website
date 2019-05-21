86c296f533b1381f51f46a767fc00b475e902abc
MirwaisAkrami <m7akrami770@gmail.com>
2019-05-21 15:20:38 +0430

    Optimize code

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,7 +10,7 @@ export class DataService {
   nodeapi = 'http://172.16.222.81:6001/api/';
 
   // For accessing the real server
-  api = 'http://nsia.gov.af:8080/index.php/wp-json/wp/v2/';
+  api = 'https://nsia.gov.af:8080/index.php/wp-json/wp/v2/';
   // nodeapi = 'http://www.nsia.gov.af:6001/api/';
   language = 'en';
   serviceType = 'stats';
