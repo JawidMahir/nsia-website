d940a856757a6367f64ad9c7fac1c5ec85d0ca90
sediqamousavi <sediqa.mousavi@gmail.com>
2019-06-23 15:08:38 +0430

    Resovle structure bug

--- node-api/server.js
+++ node-api/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require( "./app");
 
 //Use system configuration for port or use 6001 by default.
-const port = process.env.port || 6001;
+const port = process.env.port || 6002;
 
 //Create server with exported express app
 const server = http.createServer(app);
