af29c401d4950a4dadf1e5e42897003376d2f7d4
sediqamousavi <sediqa.mousavi@gmail.com>
2019-10-16 12:47:19 +0430

    Resolve procurement issue

--- src/app/opportunities/procurements/procurements.component.ts
+++ src/app/opportunities/procurements/procurements.component.ts
@@ -13,40 +13,30 @@ export class ProcurementsComponent implements OnInit {
   tenders = [];
   contents = [];
   lang;
-
   constructor(private oppService: OppService) { }
 
   ngOnInit() {
     this.getTenders(this.p);
     this.lang = localStorage.getItem('lang');
   }
-
-  getBrief(ds) {
-    if (ds.length > 40) {
-      return ds.substring(0, 39) + '...';
-    }
-    return ds;
-  }
-
   getTenders(page) {
     const customParams = [];
     customParams.push('id');
     customParams.push('date');
-    customParams.push('slug');
     customParams.push('title.rendered');
     customParams.push('content.rendered');
-    customParams.push('acf');
+    customParams.push('acf.closing_date');
     if ((this.tenders.length < 1) || (this.tenders.filter(d => d.page === this.p)).length < 1) {
       this.oppService.getTenders(customParams, page).subscribe((data) => {
         this.total = Number(data.headers.get('X-WP-Total'));
 
         const newData = {
-          page : page,
+          page: page,
           data: this.refineData(data.body)
         };
 
         this.tenders.push(newData);
-        console.log(this.tenders);
+        //console.log(this.tenders);
         this.contents = newData.data;
 
       });
@@ -54,22 +44,33 @@ export class ProcurementsComponent implements OnInit {
       this.contents = (this.tenders.filter(d => d.page === this.p))[0].data;
     }
   }
+  getBrief(ds) {
+    if (ds.length > 40) {
+      return ds.substring(0, 39) + '...';
+    }
+    return ds;
+  }
 
   refineData(data) {
     for (const tn of data) {
-      tn.content.rendered = this.oppService.htmlToPlaintext(tn.content.rendered);
+      if (tn.content) {
+        tn.content.rendered = this.oppService.htmlToPlaintext(tn.content.rendered);
+      }
       const date = new Date(tn.date);
       const tempDate = date.getDate() + '/' + (Number(date.getMonth()) + 1) + '/' + date.getFullYear();
       tn.date = tempDate;
-      if (this.lang == 'fa' || this.lang == 'ps' ) {
-        tn.date = moment(tn.date,'DD/MM/YYYY').locale('fa').format('YYYY/MM/DD');
-        tn.acf.closing_date = moment(tn.acf.closing_date,'DD/MM/YYYY').locale('fa').format('YYYY/MM/DD');
+      if (this.lang == 'fa' || this.lang == 'ps') {
+        tn.date = moment(tn.date, 'DD/MM/YYYY').locale('fa').format('YYYY/MM/DD');
+        tn.acf.closing_date = moment(tn.acf.closing_date, 'DD/MM/YYYY').locale('fa').format('YYYY/MM/DD');
       }
     }
     return data;
   }
+
   pageChanged(page: number) {
     this.p = page;
     this.getTenders(page);
   }
+
+
 }
